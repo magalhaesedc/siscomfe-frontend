26cf7b910f6eb12054cfacac918a9ff09b7ca11e
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-08-23 00:58:51 -0300

    Primeira parte do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/pages/Home.js";
+import CadastrarOferta from "./components/pages/CadastrarOferta.js";
+import Container from "./components/layout/Container.js";
+import Navbar from "./components/layout/Navbar.js";
+import Footer from "./components/layout/Footer.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/oferta" element={<CadastrarOferta />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
