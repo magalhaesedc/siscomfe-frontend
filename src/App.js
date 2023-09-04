ac3576ddf3f51b4957178fe36ef2dbd78b46c56a
magalhaesedc <edsonmagalhaesdacosta@gmail.com>
2023-09-04 20:16:15 -0300

    Implementacao de Navbar, Footer e Listagem de Ofertas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./components/pages/Home.js";
 import CadastrarOferta from "./components/pages/CadastrarOferta.js";
+import DetalhesOferta from "./components/pages/DetalhesOferta.js";
 import Container from "./components/layout/Container.js";
 import Navbar from "./components/layout/Navbar.js";
 import Footer from "./components/layout/Footer.js";
@@ -13,7 +14,8 @@ function App() {
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/oferta" element={<CadastrarOferta />} />
+          <Route path="/oferta" element={<CadastrarOferta />} />
+          <Route path="/detalhes/:id" element={<DetalhesOferta />} />
         </Routes>
       </Container>
       <Footer />
